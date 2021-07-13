4ae09df323491113fdd92fa75e99a4e5e84c41bf
Fatih Rahman Mert <fatihmert3858@gmail.com>
2021-07-13 15:30:57 +0300

    implements SystemError class

---
+++ src/domain/errors/system-error.ts
@@ -0,0 +1,30 @@
+import SystemResultCode from "../enums/system-result-code";
+
+export interface ISystemError extends Error {
+    code: SystemResultCode
+    status: number
+    data: any
+}
+
+
+class SystemError implements ISystemError {
+    data: any;
+    code: SystemResultCode;
+    status: number;
+    name: string;
+    message: string;
+    stack?: string | undefined;
+
+    constructor(message: string, code: SystemResultCode, status: number, data: any = null) {
+        this.code = code;
+        this.status = status;
+        this.data = data;
+        this.message = message;
+        this.name = this.constructor.name;
+        Error.captureStackTrace(this, this.constructor);
+
+        console.error(`Error Message : ${message}, the data is -> `, data)
+    }
+}
+
+export default SystemError;
