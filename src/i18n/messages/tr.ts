0d08e614408647f87a72f43a16cc450400f3a395
Fatih Rahman Mert <fatihmert3858@gmail.com>
2021-07-13 17:27:16 +0300

    implements languages

---
+++ src/i18n/messages/tr.ts
@@ -0,0 +1,10 @@
+import IWords from "./word";
+
+
+const TR : IWords = {
+    NOT_FOUND_API_KEY: "Api Key Bulunamadı",
+    INVALID_API_KEY: "Geçersiz Api Key!",
+    BAD_REQUEST: 'Geçersiz İstek!'
+}
+
+export default TR;
