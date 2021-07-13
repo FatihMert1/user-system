0235c7ed6ebfbeb6416820404e205526a13aaa38
Fatih Rahman Mert <fatihmert3858@gmail.com>
2021-07-13 15:35:48 +0300

    refactor app.ts file

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 import express from 'express';
+import dotenv from 'dotenv';
 
 import db from './db/db';
+import checkXApiKey from './middlewares/check-middleware';
+import handleError from './middlewares/error-handler';
+
+
+dotenv.config();
 
 const app = express();
 
 app.use(express.urlencoded({extended: true}))
+
+app.use(checkXApiKey);
 
 
 app.get('/', async (req, res) => {
@@ -16,6 +24,9 @@ app.get('/', async (req, res) => {
     res.end();
 })
 
+
+
+app.use(handleError);
 
 
 app.listen(8080, () => {
