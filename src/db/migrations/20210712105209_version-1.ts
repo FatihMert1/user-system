cb05669bb3993c7fb8c3fa6f547e7218e88c7cf8
Fatih Rahman Mert <fatihmert3858@gmail.com>
2021-07-15 17:05:36 +0300

    refactores version-1 migration

--- src/db/migrations/20210712105209_version-1.ts
+++ src/db/migrations/20210712105209_version-1.ts
@@ -1,16 +1,116 @@
-import { randomUUID } from "crypto";
 import { Knex } from "knex";
+import Gender from "../../domain/enums/gender";
 
 
 export async function up(knex: Knex): Promise<void> {
 
-    await knex.schema.createTable('roles', (builder: Knex.CreateTableBuilder) => {
-        builder.uuid('uuid').primary().unique().notNullable().defaultTo(randomUUID());
-        builder.string('role_type').unique();
+
+    await knex.schema.createTable('users', (builder: Knex.CreateTableBuilder) => {
+
+        builder.increments('id').primary();
         builder.date('created_at').notNullable().defaultTo(new Date().toLocaleDateString())
         builder.date('updated_at');
         builder.boolean('is_deleted').notNullable().defaultTo(false);
+        
+        builder.string('name').notNullable();
+        builder.string('surname').notNullable();
+        builder.integer('age').notNullable();
+        builder.string('email',255).notNullable();
+        builder.string('public_address').notNullable();
+        builder.string('password').notNullable();
+        builder.string('password_salt').notNullable();
+        builder.string('phone_number').notNullable();
+        builder.integer('gender').notNullable().defaultTo(Gender.NONE);
+
     });
+
+    await knex.schema.createTable('employees', (builder: Knex.CreateTableBuilder) => {
+
+        builder.increments('id').primary();
+        builder.date('created_at').notNullable().defaultTo(new Date().toLocaleDateString())
+        builder.date('updated_at');
+        builder.boolean('is_deleted').notNullable().defaultTo(false);
+        
+        builder.integer('user_id').notNullable().references('users.id');
+        builder.string('identification_number').notNullable();
+    });
+
+    await knex.schema.createTable('roles', (builder: Knex.CreateTableBuilder) => {
+        builder.increments('id').primary();
+        builder.date('created_at').notNullable().defaultTo(new Date().toLocaleDateString())
+        builder.date('updated_at');
+        builder.boolean('is_deleted').notNullable().defaultTo(false);
+
+        builder.integer('role_type').notNullable();
+    });
+
+    await knex.schema.createTable('customer_admins', (builder: Knex.CreateTableBuilder) => {
+
+        builder.increments('id').primary();
+        builder.date('created_at').notNullable().defaultTo(new Date().toLocaleDateString())
+        builder.date('updated_at');
+        builder.boolean('is_deleted').notNullable().defaultTo(false);
+        
+        builder.integer('employee_id').notNullable().references('employees.id');
+    });
+
+    await knex.schema.createTable('customers', (builder: Knex.CreateTableBuilder) => {
+
+        builder.increments('id').primary();
+        builder.date('created_at').notNullable().defaultTo(new Date().toLocaleDateString())
+        builder.date('updated_at');
+        builder.boolean('is_deleted').notNullable().defaultTo(false);
+        
+        builder.integer('user_id').notNullable().references('users.id');
+    });
+
+    await knex.schema.createTable('role_groups', (builder: Knex.CreateTableBuilder) => {
+
+        builder.increments('id').primary();
+        builder.date('created_at').notNullable().defaultTo(new Date().toLocaleDateString())
+        builder.date('updated_at');
+        builder.boolean('is_deleted').notNullable().defaultTo(false);
+        
+        
+        builder.string('role_group_name').notNullable();
+    });
+
+    await knex.schema.createTable('role_actions', (builder: Knex.CreateTableBuilder) => {
+
+        builder.increments('id').primary();
+        builder.date('created_at').notNullable().defaultTo(new Date().toLocaleDateString())
+        builder.date('updated_at');
+        builder.boolean('is_deleted').notNullable().defaultTo(false);
+        
+        builder.integer('role_value').notNullable();
+        builder.string('role_name').notNullable();
+        builder.integer('role_group_id').notNullable().references('role_groups.id');
+    });
+
+   
+
+    await knex.schema.createTable('role_permissions', (builder: Knex.CreateTableBuilder) => {
+
+        builder.increments('id').primary();
+        builder.date('created_at').notNullable().defaultTo(new Date().toLocaleDateString())
+        builder.date('updated_at');
+        builder.boolean('is_deleted').notNullable().defaultTo(false);
+        
+        builder.integer('role_id').notNullable().references('roles.id');
+        builder.integer('role_group_id').notNullable().references('role_groups.id');
+        builder.integer('roles_value').notNullable();
+    });
+
+    await knex.schema.createTable('super_admins', (builder: Knex.CreateTableBuilder) => {
+
+        builder.increments('id').primary();
+        builder.date('created_at').notNullable().defaultTo(new Date().toLocaleDateString())
+        builder.date('updated_at');
+        builder.boolean('is_deleted').notNullable().defaultTo(false);
+        
+        builder.integer('user_id').notNullable().references('users.id');
+    });
+
 }
 
 
